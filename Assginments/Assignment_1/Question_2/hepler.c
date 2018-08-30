d2e8f20b272ae14279822224d1b6c5aa55b5f628
Prasad Yeole <prasad.progdesk@gmail.com>
2018-08-30 16:01:28 +0530

    Assgnment_1 Question_2

---
+++ Assginments/Assignment_1/Question_2/hepler.c
@@ -0,0 +1,11 @@
+#include"header.h"
+
+void display(int iNo)
+{
+	int i = 0;
+	for(i = 1; i <= iNo; i++)
+	{
+		printf("Marvellous \n");
+	}
+	printf("\n");
+}
---
+++ Assginments/Assignment_1/Question_2/mainfile.c
@@ -0,0 +1,12 @@
+#include"header.h"
+
+int main(int argc, int *argv[])
+{
+	int iNo = 0;
+	printf("Enter the Number : ");
+	scanf("%d",&iNo);
+
+	display(iNo);	
+
+	return 0;
+}
