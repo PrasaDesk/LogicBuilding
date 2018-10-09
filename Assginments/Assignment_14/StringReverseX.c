d125b6f27e376bb063fdf070174f7c954c155b16
Prasad Yeole <prasad.progdesk@gmail.com>
2018-10-10 00:37:43 +0530

    StringReversX change

--- Assginments/Assignment_14/StringReverseX.c
+++ Assginments/Assignment_14/StringReverseX.c
@@ -3,21 +3,40 @@
 void StrReverseX(char *str)
 {
 	char temp;
-	int i = 0,j = 0;
+	int i = 0,s = 0,e = 0,j = 0;
 	if(NULL == str)
 	{
 		printf("INVALID INPUT\n");
 		return;
 	}
 
-	j = strlen(str) - 1;
+	//j = strlen(str) - 1;
 
-	while(i < j)
+	while(str[i] != '\0')
 	{
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-		i++;
-		j--;
+		if(str[i] == ' ')
+		{
+			while((str[i] == ' ')&&(str[i] != '\0'))
+			{
+				i++;
+			}
+		}
+		if(str[i] != ' ')
+		{
+			s = i;
+			while((str[i] != ' ')&&(str[i] != '\0'))
+			{
+				i++;
+			}
+			e = i-1;
+			while(s < e)
+			{
+				temp = str[s];
+				str[s] = str[e];
+				str[e] = temp;
+				s++;
+				e--;
+			}
+		}
 	}	
 }
